28d3676a27764b0bbd2f49dd953ec8d8391c1067
carloskobashi <44390065+carloskobashi@users.noreply.github.com>
2026-09-23 10:11:02 -0600

    Update config.h

--- YG_04/config.h
+++ YG_04/config.h
@@ -5,17 +5,17 @@
 // WIFI
 //======================================================
 
-#define WIFI_SSID      "RPK_2"
-#define WIFI_PASSWORD  "r3pl4st1k4"
+#define WIFI_SSID      "***"
+#define WIFI_PASSWORD  "***"
 
 //======================================================
 // INFLUXDB
 //======================================================
 
-#define INFLUXDB_URL      "http://192.168.1.10:8086"
-#define INFLUXDB_TOKEN    "uEf9j8VytIDlvUEQ9td6_feZaVHPtgjwv8Q4GGahioNrW-qVId6_SmXrdBUq3FPCfoGC0oILtMoW6eUcJ1JKsw=="
-#define INFLUXDB_ORG      "96d3ad090d8d4966"
-#define INFLUXDB_BUCKET   "centrifuga"
+#define INFLUXDB_URL      "***"
+#define INFLUXDB_TOKEN    "***"
+#define INFLUXDB_ORG      "***"
+#define INFLUXDB_BUCKET   "***"
 
 //======================================================
 // MODBUS / RS485
